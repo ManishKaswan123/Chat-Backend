5f74bdf2328ac90fa352f1f21d12179412ff6ab9
manish <manishkaswan88@gmail.com>
2024-09-21 11:00:23 +0530

    New token commit

--- controller/userDetails.js
+++ controller/userDetails.js
@@ -2,7 +2,7 @@ const getUserDetailsFromToken = require("../helpers/getUserDetailsFromToken");
 
 const userDetails = async (req, res) => {
     try {
-        const token = req.body;
+        const {token} = req.body;
         const user = await getUserDetailsFromToken(token);
 
         return res.status(200).json({
